c0a2fcfeddc23dd799f789247be891ffff305e41
kovey <kovey@vip.qq.com>
2022-12-01 16:53:03 +0800

    bugs fixed

--- lua/configs/startscreen.lua
+++ lua/configs/startscreen.lua
@@ -1,11 +1,29 @@
 -- aleph is a stripped down version of goolord/alpha-nvim to improve startup time
 local M = {}
 function M.config()
-	local aleph = require("aleph")
-	local dashboard = require("aleph.themes.dashboard")
-	aleph.setup(dashboard.opts)
-	-- Disable folding on aleph buffer:
-	vim.cmd("autocmd FileType aleph setlocal nofoldenable")
+	local dh = require("dashboard")
+    dh.custom_header = {
+        ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+        ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+        ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+        ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+        ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+        ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+    }
+    dh.custom_center = {
+        {icon = '  ', desc = 'Recently opened files                    ', shortcut = 'n h', action = 'Telescope oldfiles'},
+        {icon = '  ', desc = 'Find  File                               ', shortcut = 'n f', action = 'Telescope find_files'},
+        {icon = '  ', desc = 'File Browser                             ', shortcut = 'n t', action = 'NvimTreeToggle'},
+        {icon = '  ', desc = 'Find  word                               ', shortcut = 'n w', action = 'Telescope live_grep'},
+        {icon = '  ', desc = 'Find  string                             ', shortcut = 'n s', action = 'Telescope grep_string'},
+        {icon = "  ", desc = 'Edit config                              ', shortcut = "n e", action = "edit $MYVIMRC"}
+    }
+
+    dh.custom_footer = function()
+        local footer = {'config by kovey', 'IDE with neovim'}
+        print(footer)
+        return footer
+    end
 end
 
 return M
